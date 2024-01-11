65eaa4ced6df227b5dd7de10d7108902c9d48505
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-01-11 10:20:26 +0300

    Creating variables: Int and Double

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        double valDouble = 1234.5678;
+        System.out.println(valDouble);
+        int valInt = -2147483648;
+        System.out.println(valInt);
     }
 }
