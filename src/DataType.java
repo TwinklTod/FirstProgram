ad806e1cbfb78c658070c9c1a764d0848dbee27d
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-01-18 09:25:59 +0300

    Задача №2_Типы данных

---
+++ src/DataType.java
@@ -0,0 +1,8 @@
+public class DataType {
+    public static void DataType(){
+        double valDouble = 1234.5678;
+        System.out.println(valDouble);
+        int valInt = -2147483648;
+        System.out.println(valInt);
+    }
+}
