bf7dd37be865b57d0a500cfaf69acbeb2f3859d5
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-01-18 09:26:06 +0300

    Задача №3_Массивы

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void Arrays (){
+        int[] array = {10, 20, 30, 40, 50};
+
+        int temp = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
+
+        int sum = array[0] + array[array.length / 2];
+        System.out.println("Сумма первого и среднего элемента: " + sum);
+    }
+}
