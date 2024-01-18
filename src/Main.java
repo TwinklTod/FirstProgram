6282ca120b7777bda9ba2a6685066421c7538005
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-01-18 09:26:23 +0300

    Обновление заданий

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        double valDouble = 1234.5678;
-        System.out.println(valDouble);
-        int valInt = -2147483648;
-        System.out.println(valInt);
+        HelloWorld.HelloWorld(); //Задача №1_Первая программа
+        DataType.DataType(); // Задача №2_Типы данных
+        Arrays.Arrays(); // Задача №3_Массивы
     }
 }
