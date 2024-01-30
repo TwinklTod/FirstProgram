35428928e5d501d5fd47df8f6da19274d062a668
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-01-30 08:28:17 +0300

    Обновление задач

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача №1_Первая программа");
+        /*System.out.println("Задача №1_Первая программа");
         HelloWorld.HelloWorld(); //Задача №1_Первая программа
         System.out.println("Задача №2_Типы данных");
         DataType.DataType(); // Задача №2_Типы данных
@@ -9,6 +9,8 @@ public class Main {
         System.out.println("Задача №4_Операторы");
         AvgCalc.AvgCalc(); // Задача №4_Операторы
         System.out.println("Задача №5_Операторы");
-        Calculator.Calc(); // Задача №5_Операторы
+        Calculator.Calc(); // Задача №5_Операторы*/
+        System.out.println("Задача №6_Операторы");
+        Converter.Conv();//Задача №6_Операторы
     }
 }
