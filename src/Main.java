398742b299cf46abc73f775764900b87f459452c
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-02-03 09:33:00 +0300

    Обновление задач

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println("Задача №1_Первая программа");
+        System.out.println("Задача №1_Первая программа");
         HelloWorld.HelloWorld(); //Задача №1_Первая программа
         System.out.println("Задача №2_Типы данных");
         DataType.DataType(); // Задача №2_Типы данных
@@ -25,7 +25,7 @@ public class Main {
         System.out.println("Задача №12_Работа с данными");
         StringOperations.StrOp();//Задача №12_Работа с данными
         System.out.println("Задача №13_ Работа с данными");
-        LatinWords.LatWord();//Задача №13_ Работа с данными*/
+        LatinWords.LatWord();//Задача №13_ Работа с данными
         System.out.println("Задача №14_ Работа с данными");
         RandomArray.RndArr();//Задача №14_ Работа с данными
     }
