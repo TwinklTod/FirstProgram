a041f8be8093015144a304d8c4099febfba60900
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-02-03 17:49:51 +0300

    Итоговое задание

--- src/Summary/Main/Main.java
+++ src/Summary/Main/Main.java
@@ -5,8 +5,8 @@ import Summary.EquationSolve;
 
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println("Итоговая задача №1");
-        Converter.conv();//Итоговая задача №1*/
+        System.out.println("Итоговая задача №1");
+        Converter.conv();//Итоговая задача №1
         System.out.println("Итоговая задача №2");
         EquationSolve.equation();//Итоговая задача №2
     }
