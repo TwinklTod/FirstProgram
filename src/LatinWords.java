1acd67e4ba7f53118d4b61c7afb0f0e5f5b1db3e
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-02-03 09:13:41 +0300

    Задача №13_ Работа с данными

---
+++ src/LatinWords.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LatinWords {
+    public static void LatWord() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку из слов, разделенных пробелами: ");
+        String inputString = scanner.nextLine();
+
+        System.out.println("Слова из латинских символов:");
+
+        String[] words = inputString.split("\\s+");
+        int latinWordsCount = 0;
+
+        for (String word : words) {
+            if (isLatin(word)) {
+                System.out.println(word);
+                latinWordsCount++;
+            }
+        }
+
+        System.out.println("Количество слов из латинских символов: " + latinWordsCount);
+    }
+    private static boolean isLatin(String word) {
+        return word.matches("[a-zA-Z]+");
+    }
+}
