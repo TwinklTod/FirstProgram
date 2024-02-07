6c51871d41dfe10e1ccc0fdd3a05d1a4f87bc988
Скорняков Евгений Александрович <eskornyakov@ibs.ru>
2024-02-07 10:32:55 +0300

    Обновление задач

--- src/Summary/Main/Main.java
+++ src/Summary/Main/Main.java
@@ -2,6 +2,7 @@ package Summary.Main;
 
 import Summary.Converter;
 import Summary.EquationSolve;
+import Summary.MaxDistinctChars;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,8 @@ public class Main {
         Converter.conv();//Итоговая задача №1
         System.out.println("Итоговая задача №2");
         EquationSolve.equation();//Итоговая задача №2
+        System.out.println("Итоговая задача № 3");
+        MaxDistinctChars.maxChars();//Итоговая задача № 3
     }
 }
 
